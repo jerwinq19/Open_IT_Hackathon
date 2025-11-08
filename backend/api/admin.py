2f6071dfbb63d53025c105e42ecae89cdfc48e2f
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2025-11-08 21:52:59 +0800

    add one call loc

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import EarthquakeInfo
+from .models import EarthquakeInfo, Report
 # Register your models here.
 
 
 admin.site.register(EarthquakeInfo)
+admin.site.register(Report)
